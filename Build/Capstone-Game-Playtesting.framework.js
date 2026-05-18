dc7a8633a6dd7f42541aeef1aa26ef6e073a1a77
Aidan <aidanlyum808@gmail.com>
2026-05-17 23:04:34 -0700

    Push push push

--- Build/Capstone-Game-Playtesting.framework.js
+++ Build/Capstone-Game-Playtesting.framework.js
@@ -1334,10 +1334,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  4686824: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 4686879: ($0) => { performance.now = function() { return $0; }; },  
- 4686927: ($0) => { performance.now = function() { return $0; }; },  
- 4686975: () => { performance.now = Module['emscripten_get_now_backup']; }
+  4687208: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 4687263: ($0) => { performance.now = function() { return $0; }; },  
+ 4687311: ($0) => { performance.now = function() { return $0; }; },  
+ 4687359: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/Capstone-Game-Playtesting.loader.js
+++ Build/Capstone-Game-Playtesting.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 63896276,
+    wasmFileSize: 63900753,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
